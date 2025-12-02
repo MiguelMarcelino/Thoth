217c0ca0c585331e5ccf017ce344d14f56e7df51
Miguel Marcelino <mac.marcelino@outlook.pt>
2025-12-02 08:55:40 +0000

    Add longest substring

--- algorithms-and-data-structures/implementations.py
+++ algorithms-and-data-structures/implementations.py
@@ -552,3 +552,25 @@ def subarray_sum(nums, k):
 
     return count
 
+
+###########################################
+# Longest substring without repeating characters
+###########################################
+
+def lengthOfLongestSubstring(s: str) -> int:
+    # Store last index of each character
+    last_seen = {}
+
+    left = 0
+    max_len = 0
+
+    for right, char in enumerate(s):
+        if char in last_seen and last_seen[char] >= left:
+            left = last_seen[char] + 1
+
+        last_seen[char] = right
+
+        max_len = max(max_len, right - left + 1)
+
+    return max_len
+
